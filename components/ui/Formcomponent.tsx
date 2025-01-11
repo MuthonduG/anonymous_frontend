e7fae7e1af7244d9cf27cb188c2c14595b0699bf
MuthonduG <muthondugithinji@gmail.com>
2025-01-11 05:55:59 -0500

    initial commit

---
+++ components/ui/Formcomponent.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Formcomponent = () => {
+  return (
+    <div>Formcomponent</div>
+  )
+}
+
+export default Formcomponent
---
+++ components/ui/Imageholder.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Imageholder = () => {
+  return (
+    <div>Imageholder</div>
+  )
+}
+
+export default Imageholder
